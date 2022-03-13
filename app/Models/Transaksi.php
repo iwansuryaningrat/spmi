ee4d65c7c6fbcbdad4c2a4575865971c7801cd58
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-03-14 01:19:43 +0700

    Create Transaksi Model

---
+++ app/Models/Transaksi.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Transaksi extends Model
+{
+    protected $table            = 'transaksi';
+    protected $primaryKey       = 'transaksi_id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $allowedFields    = ["user_id", "unit_id", "created_at", "updated_at"];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+}
