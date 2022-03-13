a109384eb88decebb911b76c0c539eebbb2dc59b
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-03-14 01:50:33 +0700

    Edit controllers

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -4,8 +4,39 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 
+use App\Models\UnitsModel;
+use App\Models\TransaksiModel;
+use App\Models\KategoriModel;
+use App\Models\TahunModel;
+use App\Models\UsersModel;
+use App\Models\DataIndukModel;
+use App\Models\IndikatorModel;
+use App\Models\StandarModel;
+
+
 class Admin extends BaseController
 {
+    protected $unitsModel;
+    protected $transaksiModel;
+    protected $kategoriModel;
+    protected $tahunModel;
+    protected $usersModel;
+    protected $dataIndukModel;
+    protected $indikatorModel;
+    protected $standarModel;
+
+    public function __construct()
+    {
+        $this->unitsModel = new UnitsModel();
+        $this->transaksiModel = new TransaksiModel();
+        $this->kategoriModel = new KategoriModel();
+        $this->tahunModel = new TahunModel();
+        $this->usersModel = new UsersModel();
+        $this->dataIndukModel = new DataIndukModel();
+        $this->indikatorModel = new IndikatorModel();
+        $this->standarModel = new StandarModel();
+    }
+
     public function index()
     {
         //
