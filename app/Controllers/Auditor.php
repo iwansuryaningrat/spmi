ce49e427ae9f59456ead7133bbe6ef3d7905f8cb
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-03-14 01:38:16 +0700

    Create some controllers

---
+++ app/Controllers/Auditor.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Auditor extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
---
+++ app/Controllers/Leader.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Leader extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
