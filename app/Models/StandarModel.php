3c74ae1ce729ed895639acad1a1b28c37ea1bc76
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-03-14 01:28:28 +0700

    Create Standar Model

--- app/Models/StandarModel.php
+++ app/Models/StandarModel.php
@@ -6,37 +6,15 @@ use CodeIgniter\Model;
 
 class StandarModel extends Model
 {
-    protected $DBGroup          = 'default';
-    protected $table            = 'standars';
-    protected $primaryKey       = 'id';
+    protected $table            = 'standar';
+    protected $primaryKey       = 'standar_id';
     protected $useAutoIncrement = true;
-    protected $insertID         = 0;
     protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ["nama_standar", "status", "nilai", "kategori_id", "tahun_id", "unit_id", "created_at", "updated_at"];
 
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
 }
