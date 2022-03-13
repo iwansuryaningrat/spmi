e58abcbedbf410ad163ea3ca18de0bc14b4012ee
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-03-14 01:23:13 +0700

    Create DataInduk Model

---
+++ app/Models/DataInduk.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class DataInduk extends Model
+{
+    protected $table            = 'data_induk';
+    protected $primaryKey       = 'induk_id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $allowedFields    = ["kategori_id", "kebutuhan", "nilai", "tahun_id", "unit", "created_at", "updated_at"];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+}
