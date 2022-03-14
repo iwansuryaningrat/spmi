0a7ca98701ae89f3bce7172360c859fdb7ea8e27
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-03-14 11:44:47 +0700

    Create some method at Users Model

--- app/Models/UsersModel.php
+++ app/Models/UsersModel.php
@@ -17,4 +17,22 @@ class UsersModel extends Model
     protected $dateFormat    = "datetime";
     protected $createdField  = "created_at";
     protected $updatedField  = "updated_at";
+
+    // function to get data by email
+    public function getUserEmail($email)
+    {
+        return $this->where('email', $email)->first();
+    }
+
+    // function to get data by role
+    public function getUserRole($role)
+    {
+        return $this->where('role', $role)->first();
+    }
+
+    // function to get data by id
+    public function getUserId($id)
+    {
+        return $this->where('user_id', $id)->first();
+    }
 }
