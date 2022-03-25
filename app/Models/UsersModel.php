92df402278f0cdbcecef3033f19d4740bf4a2f13
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-03-26 03:12:14 +0700

    Create Users Model

---
+++ app/Models/UsersModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UsersModel extends Model
+{
+    protected $table            = 'users';
+    protected $primaryKey       = 'email';
+    protected $returnType       = 'array';
+    protected $allowedFields    = ['email', 'nama', 'nip', 'telp', 'foto', 'password', 'role_id', 'created_at', 'updated_at'];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+}
