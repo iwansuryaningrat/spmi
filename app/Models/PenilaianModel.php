4122c529a03bff1846c32181209fb7e5ca4a2292
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-03-28 03:11:10 +0700

    Finishing Indikator

--- app/Models/PenilaianModel.php
+++ app/Models/PenilaianModel.php
@@ -9,7 +9,7 @@ class PenilaianModel extends Model
     protected $table            = 'penilaian';
     protected $primaryKey       = ['tahun', "kategori_id", "standar_id", "indikator_id", "unit_id"];
     protected $returnType       = 'array';
-    protected $allowedFields    = ['tahun', "kategori_id", "standar_id", "indikator_id", "unit_id", 'nilai_input', 'dokumen', 'keterangan', 'catatan', 'status', 'hasil', 'nila_akhir', 'created_at', 'updated_at'];
+    protected $allowedFields    = ['tahun', "kategori_id", "standar_id", "indikator_id", "unit_id", 'nilai_input', 'dokumen', 'keterangan', 'catatan', 'status', 'hasil', 'nilai_akhir', 'created_at', 'updated_at'];
 
     // Dates
     protected $useTimestamps = true;
@@ -32,14 +32,20 @@ class PenilaianModel extends Model
     // Joining all tables
     public function getPenilaianSpec($unit_id, $standar_id, $tahun, $kategori_id)
     {
-        return $this->select('penilaian.*, kategori.nama_kategori, standar.nama_standar, units.*')
+        return $this->select('penilaian.*, kategori.nama_kategori, standar.nama_standar , indikator.*, units.*, unit_induk_tahun.nilai')
             ->join('kategori', 'kategori.kategori_id = penilaian.kategori_id')
-            ->join('standar', 'standar.kategori_id = penilaian.kategori_id')
+            ->join('standar', 'standar.standar_id = penilaian.standar_id')
             ->join('units', 'units.unit_id = penilaian.unit_id')
+            ->join('indikator', 'indikator.standar_id = penilaian.standar_id')
+            ->join('unit_induk_tahun', 'unit_induk_tahun.induk_id = indikator.induk_id')
             ->where('penilaian.unit_id', $unit_id)
             ->where('penilaian.standar_id', $standar_id)
             ->where('penilaian.tahun', $tahun)
             ->where('penilaian.kategori_id', $kategori_id)
+            ->where('indikator.kategori_id', $kategori_id)
+            ->where('standar.kategori_id', $kategori_id)
+            ->where('unit_induk_tahun.tahun', $tahun)
+            ->where('unit_induk_tahun.unit_id', $unit_id)
             ->findAll();
     }
 
@@ -49,6 +55,29 @@ class PenilaianModel extends Model
         return $this->where('unit_id', $unit_id)
             ->where('tahun', $tahun)
             ->set('status', $status)
+            ->update();
+    }
+
+    // Get status berdasarkan unit_id dan tahun dan standar dan kategori_id
+    public function getStatus($unit_id, $tahun, $standar_id, $kategori_id)
+    {
+        return $this->select('status')
+            ->where('unit_id', $unit_id)
+            ->where('tahun', $tahun)
+            ->where('standar_id', $standar_id)
+            ->where('kategori_id', $kategori_id)
+            ->findAll();
+    }
+
+    // Update data
+    public function updatePenilaian($unit_id, $tahun, $standar_id, $kategori_id, $indikator_id, $data)
+    {
+        return $this->where('unit_id', $unit_id)
+            ->where('tahun', $tahun)
+            ->where('standar_id', $standar_id)
+            ->where('kategori_id', $kategori_id)
+            ->where('indikator_id', $indikator_id)
+            ->set($data)
             ->update();
     }
 }
--- app/Models/UnitIndukTahunModel.php
+++ app/Models/UnitIndukTahunModel.php
@@ -17,7 +17,7 @@ class UnitIndukTahunModel extends Model
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
 
-    // Joining Unit anda Induk Table
+    // Joining Unit and Induk Table
     public function getIndukUnit($unit_id, $tahun)
     {
         return $this->select('unit_induk_tahun.*, units.*, data_induk.*, tahun.*, kategori.*')
@@ -38,5 +38,20 @@ class UnitIndukTahunModel extends Model
             ->where('induk_id', $induk_id)
             ->set('nilai', $nilai)
             ->update();
+    }
+
+    // Joining all tables
+    public function getIndukUnitSpec($unit_id, $tahun, $induk_id, $kategori_id)
+    {
+        return $this->select('unit_induk_tahun.*, units.*, data_induk.*, tahun.*, kategori.*')
+            ->join('units', 'units.unit_id = unit_induk_tahun.unit_id')
+            ->join('data_induk', 'data_induk.induk_id = unit_induk_tahun.induk_id')
+            ->join('tahun', 'tahun.tahun = unit_induk_tahun.tahun')
+            ->join('kategori', 'kategori.kategori_id = data_induk.kategori_id')
+            ->where('unit_induk_tahun.unit_id', $unit_id)
+            ->where('unit_induk_tahun.tahun', $tahun)
+            ->where('unit_induk_tahun.induk_id', $induk_id)
+            ->where('data_induk.kategori_id', $kategori_id)
+            ->first();
     }
 }
