e6453f0e989a2d22b8eb2ab1d6071af647afdf2c
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-03-28 05:48:43 +0700

    Fixing Base User

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -8,12 +8,14 @@ use App\Models\DataIndukModel;
 use App\Models\IndikatorModel;
 use App\Models\KategoriModel;
 use App\Models\PenilaianModel;
+use App\Models\RoleModel;
 use App\Models\StandarModel;
 use App\Models\TahunModel;
 use App\Models\UnitIndukTahunModel;
 use App\Models\UnitsModel;
 use App\Models\UsersModel;
 use App\Models\UserUnitModel;
+use App\Models\UserRoleUnitModel;
 use Config\Validation;
 
 class Admin extends BaseController
@@ -22,12 +24,14 @@ class Admin extends BaseController
     protected $indikatorModel;
     protected $kategoriModel;
     protected $penilaianModel;
+    protected $roleModel;
     protected $standarModel;
     protected $tahunModel;
     protected $unitIndukTahunModel;
     protected $unitsModel;
     protected $usersModel;
     protected $userunitModel;
+    protected $userroleunitModel;
 
     public function __construct()
     {
@@ -35,12 +39,14 @@ class Admin extends BaseController
         $this->indikatorModel = new IndikatorModel();
         $this->kategoriModel = new KategoriModel();
         $this->penilaianModel = new PenilaianModel();
+        $this->roleModel = new RoleModel();
         $this->standarModel = new StandarModel();
         $this->tahunModel = new TahunModel();
         $this->unitIndukTahunModel = new UnitIndukTahunModel();
         $this->unitsModel = new UnitsModel();
         $this->usersModel = new UsersModel();
         $this->userunitModel = new UserUnitModel();
+        $this->userroleunitModel = new UserRoleUnitModel();
         $this->data_user = [
             'email' => session()->get('email'),
             'nama' => session()->get('nama'),
@@ -75,112 +81,6 @@ class Admin extends BaseController
         return view('admin/index', $data);
     }
 
-    // Standar Method
-    public function standar()
-    {
-        return view('admin/standar');
-    }
-
-    // Data Induk Method
-    public function dataInduk()
-    {
-        return view('admin/dataInduk');
-    }
-
-    // units Method
-    public function units()
-    {
-        return view('admin/units');
-    }
-
-    // Profile Method
-    public function profile()
-    {
-        return view('admin/profile');
-    }
-
-    // User Method (Done)
-    public function user()
-    {
-        $usersession = $this->data_user;
-
-        $user = $this->usersModel->getUserRole('user');
-
-        $data = [
-            'title' => 'Base User | SIPMPP Admin UNDIP',
-            'tab' => 'user',
-            'css' => 'styles-admin-user.css',
-            'header' => 'header__mini',
-            'i' => $this->i,
-            'usersession' => $usersession,
-            'users' => $user
-        ];
-
-        return view('admin/user-baseuser', $data);
-    }
-
-    // Leader Method
-    public function leader()
-    {
-        return view('admin/user-leader');
-    }
-
-    // Auditor Method
-    public function auditor()
-    {
-        return view('admin/user-auditor');
-    }
-
-    // Report Method
-    public function report()
-    {
-        return view('admin/report');
-    }
-
-
-
-    //Add Unit Method
-    public function addUnit()
-    {
-        return view('admin/add-units');
-    }
-
-    //view data induk method
-    public function viewDataInduk()
-    {
-        return view('admin/view-dataInduk');
-    }
-
-    //add data induk method
-    public function addDataInduk()
-    {
-        return view('admin/add-dataInduk');
-    }
-
-    //view standar method
-    public function viewStandar()
-    {
-        return view('admin/view-standar');
-    }
-
-    //Add standar method
-    public function addStandar()
-    {
-        return view('admin/add-standar');
-    }
-
-    //view indikator
-    public function viewIndikator()
-    {
-        return view('admin/view-indikator');
-    }
-
-    //penilaian
-    public function penilaian()
-    {
-        return view('admin/penilaian');
-    }
-
     //daftar user (Done)
     public function daftarUser()
     {
@@ -198,6 +98,92 @@ class Admin extends BaseController
         return view('admin/daftar-user', $data);
     }
 
+    // User Method (Kurang parsing data units)
+    public function user()
+    {
+        $usersession = $this->data_user;
+        $users = $this->userroleunitModel->getData();
+        // dd($users);
+
+        $data = [
+            'title' => 'Base User | SIPMPP Admin UNDIP',
+            'tab' => 'user',
+            'css' => 'styles-admin-user.css',
+            'header' => 'header__mini',
+            'i' => $this->i,
+            'usersession' => $usersession,
+            'users' => $users
+        ];
+
+        return view('admin/user-baseuser', $data);
+    }
+
+    // Leader Method
+    public function leader()
+    {
+        return view('admin/user-leader');
+    }
+
+    // Auditor Method
+    public function auditor()
+    {
+        return view('admin/user-auditor');
+    }
+
+    // units Method
+    public function units()
+    {
+        return view('admin/units');
+    }
+
+    // Data Induk Method
+    public function dataInduk()
+    {
+        return view('admin/dataInduk');
+    }
+
+    //view data induk method
+    public function viewDataInduk()
+    {
+        return view('admin/view-dataInduk');
+    }
+
+    // Standar Method
+    public function standar()
+    {
+        return view('admin/standar');
+    }
+
+    //view standar method
+    public function viewStandar()
+    {
+        return view('admin/view-standar');
+    }
+
+    //view indikator
+    public function viewIndikator()
+    {
+        return view('admin/view-indikator');
+    }
+
+    //penilaian
+    public function penilaian()
+    {
+        return view('admin/penilaian');
+    } // Profile Method
+    public function profile()
+    {
+        return view('admin/profile');
+    }
+
+    // Report Method
+    public function report()
+    {
+        return view('admin/report');
+    }
+
+    // FORM METHOD
+
     //Add user form method (Done)
     public function addUserForm()
     {
@@ -211,6 +197,62 @@ class Admin extends BaseController
         ];
         return view('admin/add-user', $data);
     }
+
+    // add basic user Form (Done)
+    public function addBasicUserform()
+    {
+        $users = $this->usersModel->findAll();
+        $units = $this->unitsModel->findAll();
+        $tahun = $this->tahunModel->findAll();
+        $role = $this->roleModel->getRole('user');
+        // dd($role, $tahun, $units, $users);
+        $data = [
+            'title' => 'Form Tambah User | SIPMPP Admin UNDIP',
+            'tab' => 'user',
+            'css' => 'styles-admin-add-user.css',
+            'header' => 'header__mini',
+            'i' => $this->i,
+            'usersession' => $this->data_user,
+            'users' => $users,
+            'units' => $units,
+            'tahun' => $tahun,
+            'role' => $role,
+        ];
+        return view('admin/add-base-user', $data);
+    }
+
+    // add auditor Form
+    public function addAuditor()
+    {
+        return view('admin/add-auditor');
+    }
+
+    // add leader Form
+    public function addLeader()
+    {
+        return view('admin/add-leader');
+    }
+
+    //Add Unit Method
+    public function addUnit()
+    {
+        return view('admin/add-units');
+    }
+
+    //add data induk method
+    public function addDataInduk()
+    {
+        return view('admin/add-dataInduk');
+    }
+
+    //Add standar method
+    public function addStandar()
+    {
+        return view('admin/add-standar');
+    }
+
+
+    // FORM ACTION METHOD
 
     // Add user method (Done)
     public function addUser()
@@ -250,15 +292,27 @@ class Admin extends BaseController
         }
     }
 
-    //add leader
-    public function addLeader()
+    // Add user role unit method
+    public function addBasicUser($role)
     {
-        return view('admin/add-leader');
-    }
+        $email = $this->request->getVar('user');
+        $role_id = $this->roleModel->getRole($role);
+        $role_id = (int)$role_id['role_id'];
+        $unit = $this->request->getVar('unit');
+        $tahun = (int)$this->request->getVar('tahun');
 
-    //add basic user
-    public function addBasicUser()
-    {
-        return view('admin/add-base-user');
+        $data = [
+            'email' => $email,
+            'role_id' => $role_id,
+            'unit_id' => $unit,
+            'tahun' => $tahun,
+        ];
+
+        // Insert data ke User Role Unit
+        $this->userroleunitModel->insert($data);
+
+        // Set flashdata gagal dan kirim pesan eror dengan flashdata
+        $this->session->setFlashdata('msg', '<div class="alert alert-success alert-dismissible fade show" role="alert">User berhasil ditambahkan!</div>');
+        return redirect()->to(base_url('admin/daftaruser'));
     }
 }
